dbb73e26bccc05302589f8f47efe9291217af9b5
babajideawoseemo <akinloluolaposi@gmail.com>
2024-05-02 01:19:45 +0100

    Mock Karrabo Payment Service Project

---
+++ src/main/java/paymentservice/mockpaymentservice/MockPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package paymentservice.mockpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockPaymentServiceApplication.class, args);
+	}
+
+}
